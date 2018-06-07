b2155cc97015128cfa59960f74bcc16ddbf56f50
MacStudent <macstudent@20X-iMacXX-M-428.local>
2018-06-07 15:46:03 -0400

    reverse

--- C0738092_MidTerm_MAD3464/src/c0738092_midterm_mad3464/C0738092_MidTerm_MAD3464.java
+++ C0738092_MidTerm_MAD3464/src/c0738092_midterm_mad3464/C0738092_MidTerm_MAD3464.java
@@ -15,7 +15,8 @@ public class C0738092_MidTerm_MAD3464 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LambtonStringTools lst= new LambtonStringTools();
+        lst.reverse();
     }
     
 }
---
+++ C0738092_MidTerm_MAD3464/src/c0738092_midterm_mad3464/LambtonStringTools.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c0738092_midterm_mad3464;
+
+/**
+ *
+ * @author macstudent
+ */
+public class LambtonStringTools{
+    
+    public void reverse()
+    
+    {
+        String s = "Lambton";
+        StringBuilder i= new StringBuilder();
+        i.append(s);
+        i=i.reverse();
+        System.out.println(i);
+        
+}
+}
+ 
+    
+    
+        
+        
+       
+ 
+
+            
+   
